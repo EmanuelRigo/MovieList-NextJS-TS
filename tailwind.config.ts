fd0748e685fd8e6200da6cd90641ede24c2a4ce2
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2024-06-05 20:01:39 -0300

    agregando archivos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'azul-claro': '#243cff',
+        'azul-oscuro': '#0d1b3e'
+      }
     },
   },
   plugins: [],
