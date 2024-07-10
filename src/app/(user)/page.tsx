524d390a9e295bcd89fdd365153b873648ebe782
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2024-07-10 19:01:21 -0300

    segunda opcion

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
 import { CardRow, FooterMainMenu, MainNavbar } from "@/components";
 import CardMovieViewer from "@/components/movie-viewer/CardMovieViewer";
+import { useEffect, useState } from "react";
 
 const page = () => {
+  const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="grid lg:grid-cols-3 gap3 h-screen">
       <div className="bg-zinc-500">
@@ -9,12 +25,16 @@ const page = () => {
         page11
         <FooterMainMenu />
       </div>
-      <div className="hidden lg:block bg-slate-500">
-        <CardRow />
-      </div>
-      <div className="hidden lg:block bg-orange-600">
-        <CardMovieViewer />
-      </div>
+      {screenWidth >= 1024 && (
+        <div className=" bg-slate-500">
+          <CardRow />
+        </div>
+      )}{" "}
+      {screenWidth >= 1024 && (
+        <div className=" bg-orange-600">
+          <CardMovieViewer />
+        </div>
+      )}
     </div>
   );
 };
