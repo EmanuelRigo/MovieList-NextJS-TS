045f5c5363b868c4abadcde42e100cd67f41e0fd
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2024-08-12 23:05:28 -0300

    agregando componente addButtons

--- src/components/add-movie/AddButtons.tsx
+++ src/components/add-movie/AddButtons.tsx
@@ -1,3 +1,36 @@
+import { Pelicula, MovieToAdd } from "@/interfaces/interfaces";
+
 export const AddButtons = () => {
-  return <div>AddButtons</div>;
+  return (
+    <div>
+      <div className="flex justify-start mb-4">
+        <button
+          onClick={() => handleFormatChange("vhs")}
+          className={`${
+            movieToAdd.formats.vhs ? "bg-red-400" : "bg-green-500"
+          }p-4 me-4 w-28  rounded-lg  outline outline-none hover:outline-offset-3 hover:outline-orange-500 hover:cursor-pointer  focus:text-black`}
+        >
+          VHS
+        </button>
+        <button
+          onClick={() => handleFormatChange("dvd")}
+          className="p-4 me-4 w-28 bg-gray-800 rounded-lg  outline outline-none hover:outline-offset-3 hover:outline-orange-500 hover:cursor-pointer focus:bg-orange-500 focus:text-black"
+        >
+          DVD
+        </button>
+        <button
+          onClick={() => handleFormatChange("bluray")}
+          className="p-4 w-28 bg-gray-800 rounded-lg  outline outline-none hover:outline-offset-3 hover:outline-orange-500 hover:cursor-pointer focus:bg-orange-500 focus:text-black"
+        >
+          BLU-RAY
+        </button>
+      </div>
+      <button
+        onClick={checkFormats}
+        className="p-5 bg-orange-500 rounded-lg w-full text-black"
+      >
+        Agregar pelicula
+      </button>
+    </div>
+  );
 };
